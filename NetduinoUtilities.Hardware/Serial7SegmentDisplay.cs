b50c8377ab569f5c675e99e2a8e49d1d1e4bfa28
John Ruddy <john@irishwebguy.com>
2011-09-09 15:50:46 +0100

    Added function to scroll messsage across the display

--- NetduinoUtilities.Hardware/Serial7SegmentDisplay.cs
+++ NetduinoUtilities.Hardware/Serial7SegmentDisplay.cs
@@ -1,12 +1,11 @@
-﻿namespace NetduinoUtilities.Hardware
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+
+
+namespace NetduinoUtilities.Hardware
 {
-    using System;
-    using System.IO.Ports;
-    using System.IO;
-    using System.Text;
-    
-
-
     /// <summary>
     /// Class to Interfaace with a 7 Segment Display
     /// </summary>
@@ -18,26 +17,26 @@
         /// <summary>
         /// char array used to convert a number into a hexadecimal display
         /// </summary>
-        private static readonly char[] ByteToHex = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        private static readonly char[] ByteToHex = new[]
+                                                       {
+                                                           '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
+                                                           'C',
+                                                           'D', 'E', 'F'
+                                                       };
+
+        /// <summary>
+        /// The encoding.
+        /// </summary>
+        private static readonly UTF8Encoding Encoding = new UTF8Encoding();
 
         #endregion
 
         #region Private Constant/Read-Only Members
 
         /// <summary>
-        /// 
+        /// The brightness.
         /// </summary>
         private const byte Brightness = 0x7A;
-
-        #endregion
-
-        #region Private Members
-
-        /// <summary>
-        /// 
-        /// </summary>
-        private static readonly UTF8Encoding Encoding = new UTF8Encoding();
-        private string p;
 
         #endregion
 
@@ -49,9 +48,8 @@
         /// <remarks></remarks>
         public Serial7SegmentDisplay()
         {
-            
-            Port = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
-            Port.Open();
+            this.Port = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+            this.Port.Open();
         }
 
         /// <summary>
@@ -65,6 +63,31 @@
             this.Port.Open();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Serial7SegmentDisplay"/> class.
+        /// </summary>
+        /// <param name="comPort">The COM port.</param>
+        /// <param name="parity">The parity.</param>
+        /// <remarks></remarks>
+        public Serial7SegmentDisplay(string comPort, Parity parity)
+        {
+            this.Port = new SerialPort(comPort, 9600, parity, 8, StopBits.One);
+            this.Port.Open();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Serial7SegmentDisplay"/> class.
+        /// </summary>
+        /// <param name="comPort">The COM port.</param>
+        /// <param name="parity">The parity.</param>
+        /// <param name="stopbits">The stopbits.</param>
+        /// <remarks></remarks>
+        public Serial7SegmentDisplay(string comPort, Parity parity, StopBits stopbits)
+        {
+            this.Port = new SerialPort(comPort, 9600, parity, 8, stopbits);
+            this.Port.Open();
+        }
+
         #endregion
 
         #region Public Properties
@@ -72,13 +95,9 @@
         /// <summary>
         /// Gets or sets the port.
         /// </summary>
-        /// <value>The port.</value>
+        /// <value>The serial port used for communication</value>
         /// <remarks></remarks>
-        public SerialPort Port
-        {
-            get;
-            set;
-        }
+        public SerialPort Port { get; set; }
 
         #endregion
 
@@ -93,10 +112,6 @@
             Dispose();
         }
 
-        #endregion
-
-        #region Internal Methods
-
         /// <summary>
         /// Prints the specified message.
         /// </summary>
@@ -106,10 +121,10 @@
         {
             byte[] resetDisplay = Encoding.GetBytes("v");
 
-            Port.Write(resetDisplay, 0, 1);
+            this.Port.Write(resetDisplay, 0, 1);
 
             byte[] outputMessage = Encoding.GetBytes(message);
-            Port.Write(outputMessage, 0, 4);
+            this.Port.Write(outputMessage, 0, 4);
         }
 
         /// <summary>
@@ -128,35 +143,94 @@
             this.Port.Write(buffer, 0, 2);
         }
 
-
-        public void ScrollMessage(string message, int delay, int padding);
+        /// <summary>
+        /// Scrolls the message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="delay">The delay.</param>
+        /// <remarks></remarks>
+        public void ScrollMessage(string message, int delay)
         {
-                // todo: create function to scroll a message across the screen
-                
-                // pseudocode of what we want to do
-                //display.Print("XXXX");
-                //Thread.Sleep(100);
-                //display.Print("XXXJ");
-                //Thread.Sleep(100);
-                //display.Print("XXJO");
-                //Thread.Sleep(100);
-                //display.Print("XJOH");
-                //Thread.Sleep(100);
-                //display.Print("JOHN");
-                //Thread.Sleep(100);
-                //display.Print("OHNX");
-                //Thread.Sleep(100);
-                //display.Print("HNXX");
-                //Thread.Sleep(100);
-                //display.Print("NXXX");
-                //Thread.Sleep(100);
-                //display.Print("XXXX");
-                //Thread.Sleep(100);
+            string messageString = this.CreateStringWithPadding(message, 4);
+            char[] messageChars = messageString.ToCharArray();
+
+            // output the first four characters of the string
+            int currentIndex = 0;
+
+            while (true)
+            {
+                string displayMessage = string.Empty;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    displayMessage = displayMessage + messageChars[currentIndex];
+
+                    if (currentIndex >= messageString.Length - 1)
+                    {
+                        currentIndex = 0;
+                    }
+                    else
+                    {
+                        currentIndex++;
+                    }
+                }
+
+                if (currentIndex > 3)
+                {
+                    currentIndex = currentIndex - 3;
+                }
+
+                this.Print(displayMessage);
+
+                System.Threading.Thread.Sleep(delay);
+            }
         }
 
+        /// <summary>
+        /// Creates the string with padding.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="padding">The padding.</param>
+        /// <returns>The create string with padding.</returns>
+        /// <remarks></remarks>
+        private string CreateStringWithPadding(string message, int padding)
+        {
+            char[] letters = message.ToCharArray();
+            string messageString = string.Empty;
+
+            // add a blank for any spaces in the string
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i] == ' ')
+                {
+                    // blank character
+                    letters[i] = 'X';
+                }
+
+                messageString = messageString + letters[i];
+            }
+
+            // add the padding to the string
+            string paddingString = string.Empty;
+
+            for (int i = 0; i < padding; i++)
+            {
+                paddingString = paddingString + 'X';
+            }
+
+            return paddingString + messageString + paddingString;
+        }
 
         #endregion
 
-
+        /// <summary>
+        /// Blinks the message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <remarks></remarks>
+        public void BlinkMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
